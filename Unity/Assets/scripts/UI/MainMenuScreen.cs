b3638037183e091b1de7f72364a519d384c5a83a
Daniel <messer_@hotmail.com>
2016-08-07 11:57:30 +0200

    - Added sound file-assets - Made a "Main Menu"-screen and a victory screen - Started implementing messaging system

---
+++ Unity/Assets/scripts/UI/MainMenuScreen.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPlayClick()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void OnVictoryClick()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+
+    public void OnVictoryBackClick()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+}
---
+++ Unity/Assets/scripts/_src/MessageManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public interface IMessage
+{
+}
+
+public interface IMessageListener
+{
+    void HandleMessage(IMessage msg);
+}
+
+public static class MessageManager
+{
+    private static Dictionary<Type, List<IMessageListener>> _callbacks = new Dictionary<Type, List<IMessageListener>>();
+    private static List<IMessage> _messages = new List<IMessage>();
+
+    public static void AddListener(Type type, IMessageListener listener)
+    {
+        if (!_callbacks.ContainsKey(type))
+            _callbacks.Add(type, new List<IMessageListener>() { listener });
+        else
+            _callbacks[type].Add(listener);
+    }
+
+    public static void RemoveListener(Type type, IMessageListener listener)
+    {
+        _callbacks[type].Remove(listener);
+    }
+
+    public static void QueueMessage(IMessage msg)
+    {
+        _messages.Add(msg);
+    }
+
+    public static void ProcessMessageQueue()
+    {
+        var messages = _messages.ToArray();
+        _messages.Clear();
+        foreach(var msg in messages)
+        {
+            if(_callbacks.ContainsKey(msg.GetType()))
+            {
+                var listeners = _callbacks[msg.GetType()].ToArray();
+                foreach(var listener in listeners)
+                {
+                    listener.HandleMessage(msg);
+                }
+            }
+        }
+    }
+
+    public static void Reset()
+    {
+        _callbacks.Clear();
+        _messages.Clear();
+    }
+}
